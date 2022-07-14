d81448e1ca5080d6a1e46c63d0ba8ca28986ecf5
Dianita <diane.lopez2@utsa.edu>
2022-07-14 13:45:41 -0500

    learn basic grid cmds

---
+++ grid.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+root =Tk()
+
+#creating a label widget
+myLabel1 =Label(root, text ="Hello World")
+myLabel2 =Label(root, text="my name is diane")
+myLabel3 =Label(root, text=" ")
+
+#add labels onto the screen
+
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=5)
+myLabel3.grid(row=1, column=1)
+
+root.mainloop()
--- learningTkinter.py
+++ learningTkinter.py
@@ -1,3 +1,6 @@
+#learning tkinter
+#code written by Diane Lopez
+#add grid 7/14/22
 from tkinter import *
 
 root = Tk()
@@ -9,10 +12,10 @@ def hello():
     hello_label.pack()
 
 myLabel = Label(root, text= "enter your first name:")
-myLabel.pack()
+myLabel.grid(row=0, column=0)
 
 myTextbox = Entry(root, width=30)
-myTextbox.pack()
+myTextbox.pack(row=1, column=0)
 
 myButton = Button(root,text="Submit", command=hello)
 myButton.pack()
